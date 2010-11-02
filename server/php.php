41410118e778951785172c2a8040f1dd7ae4f9b8
João Dias <alfa.project@gmail.com>
2010-11-02 07:47:06 -0700

    Changed single quotes to double quotes. Single quotes are not valid JSON.

--- server/php.php
+++ server/php.php
@@ -87,7 +87,7 @@ class qqFileUploader {
         
         if ($postSize < $this->sizeLimit || $uploadSize < $this->sizeLimit){
             $size = max(1, $this->sizeLimit / 1024 / 1024) . 'M';             
-            die("{'error':'increase post_max_size and upload_max_filesize to $size'}");    
+            die('{"error":"Increase \'post_max_size\' and/or \'upload_max_filesize\' to '. $size .'."}');
         }        
     }
     
