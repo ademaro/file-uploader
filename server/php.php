24818b08491c592c6d7709d559d4d1b804acd774
João Dias <alfa.project@gmail.com>
2010-11-02 05:36:54 -0700

    Return the filename on success, just in case it was changed.

--- server/php.php
+++ server/php.php
@@ -140,7 +140,10 @@ class qqFileUploader {
         }
         
         if ($this->file->save($uploadDirectory . $filename . '.' . $ext)){
-            return array('success'=>true);
+            return array(
+                'success'  => TRUE,
+                'filename' => $filename .'.'. $ext
+            );
         } else {
             return array('error'=> 'Could not save uploaded file.' .
                 'The upload was cancelled, or server error encountered');
